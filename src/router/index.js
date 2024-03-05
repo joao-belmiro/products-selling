f65d15720ff105988266b56299a2a23eb11206eb
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-05 19:10:18 -0300

    feat: update vue and router version :(

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,52 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '../components/Layout.vue'
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'home-view',
-        component: () => import('../views/HomeView.vue')
-      },
-      {
-        path: '/clients',
-        name: 'clients-view',
-        component: () => import('../views/ClientsView.vue')
-      },
-      {
-        path: '/new-client',
-        name: 'new-client',
-        component: () => import('../views/FormClient.vue')
-      },
-      {
-        path: '/products',
-        name: 'products-view',
-        component: () => import('../views/ProductsView.vue')
-      },
-      {
-        path: '/new-product',
-        name: 'new-product',
-        component: () => import('../views/FormProduct.vue')
-      },
-      {
-        path: '/carts',
-        name: 'carts',
-        component: () => import('../views/CartView.vue')
-      },
-      {
-        path: '/new-cart',
-        name: 'new-cart',
-        component: () => import('../views/FormCart.vue')
-      }
-    ]
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          name: 'home-view',
+          component: () => import('../views/HomeView.vue')
+        },
+        {
+          path: '/clients',
+          name: 'clients-view',
+          component: () => import('../views/ClientsView.vue')
+        },
+        {
+          path: '/new-client',
+          name: 'new-client',
+          component: () => import('../views/FormClient.vue')
+        },
+        {
+          path: '/products',
+          name: 'products-view',
+          component: () => import('../views/ProductsView.vue')
+        },
+        {
+          path: '/new-product',
+          name: 'new-product',
+          component: () => import('../views/FormProduct.vue')
+        },
+        {
+          path: '/carts',
+          name: 'carts',
+          component: () => import('../views/CartView.vue')
+        },
+        {
+          path: '/new-cart',
+          name: 'new-cart',
+          component: () => import('../views/FormCart.vue')
+        }
+      ]
+    }
+  ]
 })
 
 export default router
