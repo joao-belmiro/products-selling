015c8c25f18f27aadf54caa58a22115ff4f3a305
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-05 15:16:01 -0300

    feat: create card component

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c__card">
+    <slot></slot>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.c__card {
+  border: 1px solid $grey-2;
+  padding: 16px;
+  border-radius: 16px;
+}
+</style>
