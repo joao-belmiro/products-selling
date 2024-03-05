31fa1ef23b6c57a65ba4b65e37818cd454f68764
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-05 15:29:01 -0300

    feat: create Products screens for CRUD operations

---
+++ src/views/FormProduct.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="pa-4">
+    <card>
+      <span class="title-card">Cadastro de Produto</span>
+      <hr />
+      <br />
+      <Form
+        textCancel="Cancelar"
+        textSubmit="Salvar"
+        routeBack="/products"
+        class="form-container"
+        @onSubmit="saveOrUpadate"
+      >
+        <div class="i-fields">
+          <div class="input-group">
+            <label> Nome </label>
+            <div class="input-field">
+              <input v-model="name" type="text" required />
+            </div>
+          </div>
+          <div class="input-group">
+            <label> {{ active ? 'Ativo' : 'Inativo' }} </label>
+            <input v-model="active" type="checkbox" required />
+          </div>
+        </div>
+      </Form>
+    </card>
+  </div>
+</template>
+
+<script setup>
+import Card from '@/components/Card.vue'
+import Form from '@/components/Form.vue'
+import { ref } from 'vue'
+const name = ref('')
+const active = ref(true)
+const saveOrUpadate = () => {}
+</script>
+
+<style lang="scss">
+h3 {
+  text-align: left;
+  margin: 32px 0;
+
+  a {
+    color: $primary;
+    text-decoration: none;
+    &:hover {
+      color: $secondary;
+    }
+  }
+}
+</style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pa-4 f__container_column">
+    <HeaderOptions
+      text-back="voltar"
+      text-add="Novo Produto"
+      router-back="/"
+      router-add="/new-product"
+      placeholder="Digite o nome do Produto"
+      @emitSearch="onSearch"
+    ></HeaderOptions>
+    <Table :columns="columns" :data="data" id="nome"></Table>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import HeaderOptions from '../components/HeaderOptions.vue'
+import Table from '../components/Table.vue'
+const filter = ref('')
+const columns = ref([
+  { label: 'Nome', property: 'nome' },
+  { label: 'Idade', property: 'idade' },
+  { label: 'País', property: 'pais' }
+])
+const data = [
+  { nome: 'João', idade: 25, pais: 'Brasil' },
+  { nome: 'Maria', idade: 30, pais: 'EUA' },
+  { nome: 'Pedro', idade: 28, pais: 'França' }
+]
+const onSearch = (payload) => {
+  filter.value = payload
+}
+</script>
+
+<style lang="scss">
+.f__container_column {
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+}
+</style>
