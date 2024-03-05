c90cd86133278181688bce48bf87dc6d20cba148
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-05 15:20:55 -0300

    feat: create layout

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink exact to="/"> Cadastros </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="carts"> Carrinho </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+header {
+  width: 100%;
+  nav {
+    padding: 24px;
+    background-color: $accent;
+
+    ul {
+      display: flex;
+      list-style: none;
+      gap: 32px;
+      li {
+        letter-spacing: 3px;
+        a {
+          font-size: 1.2rem;
+          font-weight: 500;
+          color: #fff;
+          text-decoration: none;
+          @media only screen and (max-width: 768px) {
+            font-size: 14px;
+          }
+          &:hover,
+          &.router-link-exact-active {
+            border-bottom: 2px solid #fff;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Header from './Header.vue'
+</script>
+
+<style></style>
