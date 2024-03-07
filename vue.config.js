1635abcaa638addebcbcdc84d9a6ec023a428d0f
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-07 11:13:20 -0300

    fix: format vue.comfig

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -10,7 +10,7 @@ module.exports = defineConfig({
     },
     plugins: [
       new (require('webpack').DefinePlugin)({
-        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
       })
     ]
   },
@@ -23,4 +23,4 @@ module.exports = defineConfig({
       }
     }
   }
-});
+})
